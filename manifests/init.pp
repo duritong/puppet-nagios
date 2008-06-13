7a2faa981e91c0a224544cdfa502ac82686fa757
mh <mh@immerda.ch>
2008-06-13 20:17:12 +0000

    Merge commit 'puzzle/development'

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,18 @@
+#
+# nagios module
 # nagios.pp - everything nagios related
+#
 # Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
-# See LICENSE for the full license granted to you.
-# adapted and improved by admin(at)immerda.ch
-# adapted by Puzzle ITC - haerry+puppet(at)puzzle.ch
-
+# Copyright 2008, admin(at)immerda.ch
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
 
 # the directory containing all nagios configs:
 $nagios_cfgdir = '/var/lib/puppet/modules/nagios'
@@ -117,7 +126,7 @@ class nagios::base {
     Nagios_serviceextinfo <<||>>
     Nagios_timeperiod <<||>>
 
-    if defined(Class["munin::client"]) {
+    if $use_munin {
         include munin::plugins::nagios
     }
 } # end nagios::base
