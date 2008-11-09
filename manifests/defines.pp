e8a51beb825c93d327a6d035c84cd3d7fdcd3bcd
mh <mh@immerda.ch>
2008-11-09 11:42:43 +0000

    make domain to check choosable

--- manifests/defines.pp
+++ manifests/defines.pp
@@ -126,19 +126,24 @@ define nagios::service::ping(){
 #   - force: http is permanent redirect to https
 #   - only: check only https
 define nagios::service::http(
+    $check_domain = 'absent',
     $check_url = '/',
     $check_code = 'OK',
     $ssl_mode = 'false'
 ){
+    $real_check_domain = $check_domain ? {
+        'absent' => $name,
+        default => $check_domain
+    }
     case $ssl_mode {
         'force','true','only': {
             nagios::service{"https_${name}_${check_code}_${fqdn}":
-                check_command => "check_https_url_regex!${name}!${check_url}!'${check_code}'",
+                check_command => "check_https_url_regex!${real_check_domain}!${check_url}!'${check_code}'",
             }
             case $ssl_mode {
                 'force': {
                     nagios::service{"httprd_${name}_${fqdn}":
-                        check_command => "check_http_url_regex!${name}!${check_url}!'301'",
+                        check_command => "check_http_url_regex!${real_check_domain}!${check_url}!'301'",
                     }
                 }
             }
@@ -147,7 +152,7 @@ define nagios::service::http(
     case $ssl_mode {
         'false','true': {
             nagios::service{"http_${name}_${check_code}_${fqdn}":
-                check_command => "check_http_url_regex!${name}!${check_url}!'${check_code}'",
+                check_command => "check_http_url_regex!${real_check_domain}!${check_url}!'${check_code}'",
             }
         }
     }
