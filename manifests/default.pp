4247c22fc7b84d8b72da9250a056bf82a4ce12ee
Jerome Charaoui <jcharaoui@cmaisonneuve.qc.ca>
2009-12-01 13:44:07 -0500

    Correct default definition file source; remove unneeded config files

--- manifests/default.pp
+++ manifests/default.pp
@@ -4,7 +4,7 @@ define nagios::default {
         path => "${nagios_cfg_dir}/defaults/${name}.cfg",
         source => [ "puppet://$server/files/nagios/configs/${fqdn}/defaults/${name}.cfg",
                     "puppet://$server/files/nagios/configs/${operatingsystem}/defaults/${name}.cfg",
-                    "puppet://$server/files/nagios/configs/${name}.cfg",
+                    "puppet://$server/files/nagios/configs/defaults/${name}.cfg",
                     "puppet://$server/nagios/configs/${operatingsystem}/defaults/${name}.cfg",
                     "puppet://$server/nagios/configs/defaults/${name}.cfg" ],
         notify => Service['nagios'],
