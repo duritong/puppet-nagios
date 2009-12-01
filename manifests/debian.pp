3d284648c7d00d94d97c4bf20726076e81e58612
Jerome Charaoui <jcharaoui@cmaisonneuve.qc.ca>
2009-12-01 16:00:22 -0500

    use correct package name for snmp plugins on debian

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -2,7 +2,7 @@ class nagios::debian inherits nagios::base {
 
     Package['nagios'] { name => 'nagios3' }
 
-    package { [ 'nagios-plugins', 'nagios-plugins-snmp','nagios-nrpe-plugin' ]:
+    package { [ 'nagios-plugins', 'nagios-snmp-plugins','nagios-nrpe-plugin' ]:
         ensure => 'present',
         notify => Service['nagios'],
     }
