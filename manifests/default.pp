ee7599dad363f36bb5e514ae6508624378af6919
Jerome Charaoui <jcharaoui@cmaisonneuve.qc.ca>
2009-12-01 16:51:29 -0500

    qualify nagios_cfg_dir variable

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,13 +1,14 @@
 define nagios::default {
 
     file { "nagios_default_${name}" :
-        path => "${nagios_cfg_dir}/defaults/${name}.cfg",
+        path => "${nagios::nagios_cfg_dir}/defaults/${name}.cfg",
         source => [ "puppet://$server/files/nagios/configs/${fqdn}/defaults/${name}.cfg",
                     "puppet://$server/files/nagios/configs/${operatingsystem}/defaults/${name}.cfg",
                     "puppet://$server/files/nagios/configs/defaults/${name}.cfg",
                     "puppet://$server/nagios/configs/${operatingsystem}/defaults/${name}.cfg",
                     "puppet://$server/nagios/configs/defaults/${name}.cfg" ],
         notify => Service['nagios'],
+        ensure => absent,
         mode => 0644, owner => root, group => root;
     }
 
