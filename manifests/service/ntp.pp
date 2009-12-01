09cd0bea508f38ae37ea2ec869c332d5b6d2850f
mh <mh@immerda.ch>
2009-12-01 10:37:50 +0100

    adjust command

--- manifests/service/ntp.pp
+++ manifests/service/ntp.pp
@@ -2,7 +2,7 @@
 
 class nagios::service::ntp {
     nagios::service{ "check_ntp_${hostname}":
-        check_command => "check_ntp",
+        check_command => "check_ntp_time",
         host_name => $fqdn,
     }
 }
