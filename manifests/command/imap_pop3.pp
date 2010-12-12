4fe59df65853631ec78ce3c731f62097d6182563
mh <mh@immerda.ch>
2010-12-12 22:17:03 +0100

    no ssl connection for pop3

--- manifests/command/imap_pop3.pp
+++ manifests/command/imap_pop3.pp
@@ -5,7 +5,7 @@ class nagios::command::imap_pop3 {
     'check_imap_ssl':
       command_line => '$USER1$/check_imap -H $ARG1$ -p $ARG2$ -S';
     'check_pop3':
-      command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$ -S';
+      command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$';
     'check_pop3_ssl':
       command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$ -S';
     'check_managesieve':
