7d9843fae4c939670f005661d5867578d0a0827e
Gabriel Filion <gabriel@koumbit.org>
2011-02-15 19:11:01 -0500

    Avoid the "libnagios-plugin-perl" package for lenny
    This package doesn't exist on lenny.

--- manifests/nrpe/base.pp
+++ manifests/nrpe/base.pp
@@ -1,18 +1,21 @@
 class nagios::nrpe::base {
 
     if $nagios_nrpe_cfgdir == '' { $nagios_nrpe_cfgdir = '/etc/nagios' }
-    if $nagios_nrpe_dont_blame == '' { $nagios_nrpe_dont_blame = 1 }
     
     package { 	"nagios-nrpe-server": ensure => present;
 		    "nagios-plugins-basic": ensure => present;
-		    "libnagios-plugin-perl": ensure => present;
 		    "libwww-perl": ensure => present;   # for check_apache
 	    }
 
+    # Special-case lenny. the package doesn't exist
+    if $lsbdistcodename != 'lenny' {
+        package { "libnagios-plugin-perl": ensure => present; }
+    }
     
     file { [ $nagios_nrpe_cfgdir, "$nagios_nrpe_cfgdir/nrpe.d" ]: 
 	ensure => directory }
 
+    if $nagios_nrpe_dont_blame == '' { $nagios_nrpe_dont_blame = 1 }
     file { "$nagios_nrpe_cfgdir/nrpe.cfg":
 	    content => template('nagios/nrpe/nrpe.cfg'),
 	    owner => root, group => 0, mode => 644;
