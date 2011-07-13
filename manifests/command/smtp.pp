4c16852f285db60ff0006326e8bed11a2384d74f
Silvio Rhatto <rhatto@riseup.net>
2011-07-13 20:12:10 -0300

    Do not define check_ssmtp on debian

--- manifests/command/smtp.pp
+++ manifests/command/smtp.pp
@@ -6,8 +6,6 @@ class nagios::command::smtp {
             command_line => '$USER1$/check_smtp -H $ARG1$ -p $ARG2$ -S';
           'check_smtp_cert':
             command_line => '$USER1$/check_smtp -H $ARG1$ -p $ARG2$ -S -D $ARG3$';
-          'check_ssmtp':
-            command_line => '$USER1$/check_ssmtp -H $ARG1$ -p $ARG2$ -S';
           'check_ssmtp_cert':
             command_line => '$USER1$/check_ssmtp -H $ARG1$ -p $ARG2$ -S -D $ARG3$';
         }
