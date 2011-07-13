c46fa52e5b14e1c3e121d43bb3a9561158be2729
Silvio Rhatto <rhatto@riseup.net>
2011-07-13 20:19:31 -0300

    Do not define check_imap on debian

--- manifests/command/imap_pop3.pp
+++ manifests/command/imap_pop3.pp
@@ -1,14 +1,31 @@
 class nagios::command::imap_pop3 {
-  nagios_command{
-    'check_imap':
-      command_line => '$USER1$/check_imap -H $ARG1$ -p $ARG2$';
-    'check_imap_ssl':
-      command_line => '$USER1$/check_imap -H $ARG1$ -p $ARG2$ -S';
-    'check_pop3':
-      command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$';
-    'check_pop3_ssl':
-      command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$ -S';
-    'check_managesieve':
-      command_line => '$USER1$/check_tcp -H $ARG1$ -p 2000';
+  case $operatingsystem {
+      debian,ubuntu: {
+        nagios_command {
+          'check_imap_ssl':
+            command_line => '$USER1$/check_imap -H $ARG1$ -p $ARG2$ -S';
+          'check_pop3':
+            command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$';
+          'check_pop3_ssl':
+            command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$ -S';
+          'check_managesieve':
+            command_line => '$USER1$/check_tcp -H $ARG1$ -p 2000';
+        }
+      }
+      default: {
+        nagios_command {
+          'check_imap':
+            command_line => '$USER1$/check_imap -H $ARG1$ -p $ARG2$';
+          'check_imap_ssl':
+            command_line => '$USER1$/check_imap -H $ARG1$ -p $ARG2$ -S';
+          'check_pop3':
+            command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$';
+          'check_pop3_ssl':
+            command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$ -S';
+          'check_managesieve':
+            command_line => '$USER1$/check_tcp -H $ARG1$ -p 2000';
+        }
+      }
   }
+
 }
