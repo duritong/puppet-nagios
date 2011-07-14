69b346b34c981b91e068222b15a8a825f08344c2
Silvio Rhatto <rhatto@riseup.net>
2011-07-14 13:05:08 -0300

    Branching the definition for smtp, imap and pop3 commands

--- manifests/command/imap_pop3.pp
+++ manifests/command/imap_pop3.pp
@@ -1,31 +1,22 @@
 class nagios::command::imap_pop3 {
   case $operatingsystem {
-      debian,ubuntu: {
-        nagios_command {
-          'check_imap_ssl':
-            command_line => '$USER1$/check_imap -H $ARG1$ -p $ARG2$ -S';
-          'check_pop3':
-            command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$';
-          'check_pop3_ssl':
-            command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$ -S';
-          'check_managesieve':
-            command_line => '$USER1$/check_tcp -H $ARG1$ -p 2000';
-        }
+    debian,ubuntu: { }  # Debian/Ubuntu already define those checks
+    default: {
+      nagios_command {
+        'check_imap':
+          command_line => '$USER1$/check_imap -H $ARG1$ -p $ARG2$';
       }
-      default: {
-        nagios_command {
-          'check_imap':
-            command_line => '$USER1$/check_imap -H $ARG1$ -p $ARG2$';
-          'check_imap_ssl':
-            command_line => '$USER1$/check_imap -H $ARG1$ -p $ARG2$ -S';
-          'check_pop3':
-            command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$';
-          'check_pop3_ssl':
-            command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$ -S';
-          'check_managesieve':
-            command_line => '$USER1$/check_tcp -H $ARG1$ -p 2000';
-        }
-      }
+    }
   }
 
+  nagios_command {
+    'check_imap_ssl':
+      command_line => '$USER1$/check_imap -H $ARG1$ -p $ARG2$ -S';
+    'check_pop3':
+      command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$';
+    'check_pop3_ssl':
+      command_line => '$USER1$/check_pop -H $ARG1$ -p $ARG2$ -S';
+    'check_managesieve':
+      command_line => '$USER1$/check_tcp -H $ARG1$ -p 2000';
+  }
 }
--- manifests/command/smtp.pp
+++ manifests/command/smtp.pp
@@ -1,28 +1,22 @@
 class nagios::command::smtp {
   case $operatingsystem {
-      debian,ubuntu: {
-        nagios_command {
-          'check_smtp_tls':
-            command_line => '$USER1$/check_smtp -H $ARG1$ -p $ARG2$ -S';
-          'check_smtp_cert':
-            command_line => '$USER1$/check_smtp -H $ARG1$ -p $ARG2$ -S -D $ARG3$';
-          'check_ssmtp_cert':
-            command_line => '$USER1$/check_ssmtp -H $ARG1$ -p $ARG2$ -S -D $ARG3$';
-        }
+    debian,ubuntu: { }  # Debian/Ubuntu already define those checks
+    default: {
+      nagios_command {
+        'check_smtp':
+           command_line => '$USER1$/check_smtp -H $ARG1$ -p $ARG2$';
+        'check_ssmtp':
+           command_line => '$USER1$/check_ssmtp -H $ARG1$ -p $ARG2$ -S';
       }
-      default: {
-        nagios_command {
-          'check_smtp':
-            command_line => '$USER1$/check_smtp -H $ARG1$ -p $ARG2$';
-          'check_smtp_tls':
-            command_line => '$USER1$/check_smtp -H $ARG1$ -p $ARG2$ -S';
-          'check_smtp_cert':
-            command_line => '$USER1$/check_smtp -H $ARG1$ -p $ARG2$ -S -D $ARG3$';
-          'check_ssmtp':
-            command_line => '$USER1$/check_ssmtp -H $ARG1$ -p $ARG2$ -S';
-          'check_ssmtp_cert':
-            command_line => '$USER1$/check_ssmtp -H $ARG1$ -p $ARG2$ -S -D $ARG3$';
-        }
-      }
+    }
+  }
+
+  nagios_command {
+    'check_smtp_tls':
+       command_line => '$USER1$/check_smtp -H $ARG1$ -p $ARG2$ -S';
+    'check_smtp_cert':
+       command_line => '$USER1$/check_smtp -H $ARG1$ -p $ARG2$ -S -D $ARG3$';
+    'check_ssmtp_cert':
+       command_line => '$USER1$/check_ssmtp -H $ARG1$ -p $ARG2$ -S -D $ARG3$';
   }
 }
