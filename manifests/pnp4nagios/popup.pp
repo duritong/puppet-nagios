579b622e8f276b01ff0bc85c7828fb3dfb0b70ae
varac <varacanero@zeromail.org>
2012-09-17 19:54:14 +0200

    fixed conf file permissions, notify services

--- manifests/pnp4nagios/popup.pp
+++ manifests/pnp4nagios/popup.pp
@@ -1,17 +1,20 @@
 class nagios::pnp4nagios::popup inherits nagios::pnp4nagios { 
-    File['pnp4nagios-templates.cfg']{
-         path => "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/pnp4nagios-templates.cfg",
-         source => [ "puppet:///modules/site-nagios/pnp4nagios/pnp4nagios-popup-templates.cfg",
-                     "puppet:///modules/nagios/pnp4nagios/pnp4nagios-popup-templates.cfg"    ]
-    }
+  File['pnp4nagios-templates.cfg']{
+    path => "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/pnp4nagios-templates.cfg",
+    source => [ "puppet:///modules/site-nagios/pnp4nagios/pnp4nagios-popup-templates.cfg",
+               "puppet:///modules/nagios/pnp4nagios/pnp4nagios-popup-templates.cfg"    ]
+    mode   => 0644, owner => root, group => root;
+    notify => Service['nagios'],
+  }
 
-    file { "/usr/share/$nagios::nagios_packagename/htdocs/ssi":
-        ensure => directory }
+  file { "/usr/share/$nagios::nagios_packagename/htdocs/ssi":
+    ensure => directory }
 
-    file { 'status-header.ssi':
-        path => "/usr/share/$nagios::nagios_packagename/htdocs/ssi/status-header.ssi",
-        source => [ "puppet:///modules/site-nagios/pnp4nagios/status-header.ssi",
-                    "puppet:///modules/nagios/pnp4nagios/status-header.ssi"    ]
-    }
-
+  file { 'status-header.ssi':
+    path => "/usr/share/$nagios::nagios_packagename/htdocs/ssi/status-header.ssi",
+    source => [ "puppet:///modules/site-nagios/pnp4nagios/status-header.ssi",
+                "puppet:///modules/nagios/pnp4nagios/status-header.ssi"    ]
+    mode   => 0644, owner => root, group => root;
+    notify => Service['nagios'],
+  }
 }
