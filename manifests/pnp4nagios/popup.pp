ab52e4a72985268419b8b04a487442975d51aca3
varac <varacanero@zeromail.org>
2012-09-17 20:16:05 +0200

    typos again

--- manifests/pnp4nagios/popup.pp
+++ manifests/pnp4nagios/popup.pp
@@ -2,8 +2,8 @@ class nagios::pnp4nagios::popup inherits nagios::pnp4nagios {
   File['pnp4nagios-templates.cfg']{
     path => "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/pnp4nagios-templates.cfg",
     source => [ "puppet:///modules/site-nagios/pnp4nagios/pnp4nagios-popup-templates.cfg",
-               "puppet:///modules/nagios/pnp4nagios/pnp4nagios-popup-templates.cfg"    ]
-    mode   => 0644, owner => root, group => root;
+               "puppet:///modules/nagios/pnp4nagios/pnp4nagios-popup-templates.cfg" ],
+    mode   => 0644, owner => root, group => root,
     notify => Service['nagios'],
   }
 
@@ -13,8 +13,8 @@ class nagios::pnp4nagios::popup inherits nagios::pnp4nagios {
   file { 'status-header.ssi':
     path => "/usr/share/$nagios::nagios_packagename/htdocs/ssi/status-header.ssi",
     source => [ "puppet:///modules/site-nagios/pnp4nagios/status-header.ssi",
-                "puppet:///modules/nagios/pnp4nagios/status-header.ssi"    ]
-    mode   => 0644, owner => root, group => root;
+                "puppet:///modules/nagios/pnp4nagios/status-header.ssi"],
+    mode   => 0644, owner => root, group => root,
     notify => Service['nagios'],
   }
 }
