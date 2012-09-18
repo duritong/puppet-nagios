a9d74682007addd6c1a24efc7ee074a04c4bee99
varac <varacanero@zeromail.org>
2012-09-18 21:44:37 +0200

    no ::nagios_packagename variable anymore

--- manifests/pnp4nagios/popup.pp
+++ manifests/pnp4nagios/popup.pp
@@ -1,4 +1,5 @@
 class nagios::pnp4nagios::popup inherits nagios::pnp4nagios { 
+  notify {"${nagios::defaults::vars::int_nagios_cfgdir}":}
   File['pnp4nagios-templates.cfg']{
     path => "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/pnp4nagios-templates.cfg",
     source => [ "puppet:///modules/site-nagios/pnp4nagios/pnp4nagios-popup-templates.cfg",
@@ -7,11 +8,11 @@ class nagios::pnp4nagios::popup inherits nagios::pnp4nagios {
     notify => Service['nagios'],
   }
 
-  file { "/usr/share/$nagios::nagios_packagename/htdocs/ssi":
+  file { "/usr/share/nagios3/htdocs/ssi":
     ensure => directory }
 
   file { 'status-header.ssi':
-    path => "/usr/share/$nagios::nagios_packagename/htdocs/ssi/status-header.ssi",
+    path => "/usr/share/nagios3/htdocs/ssi/status-header.ssi",
     source => [ "puppet:///modules/site-nagios/pnp4nagios/status-header.ssi",
                 "puppet:///modules/nagios/pnp4nagios/status-header.ssi"],
     mode   => 0644, owner => root, group => root,
