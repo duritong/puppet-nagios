cfb91ce97243e743f5af0c417851aa730ab96f41
varac <varacanero@zeromail.org>
2013-02-27 19:47:14 +0100

    delete commented nagios_host and -service template definitions

--- manifests/defaults/pnp4nagios.pp
+++ manifests/defaults/pnp4nagios.pp
@@ -7,22 +7,4 @@ class nagios::defaults::pnp4nagios {
         'process-host-perfdata-file-pnp4nagios-bulk-npcd':
             command_line => '/bin/mv /var/lib/nagios3/host-perfdata /var/spool/pnp4nagios/npcd/host-perfdata.$TIMET$'
     }
-
-    # nagios host templates
-    # http://docs.pnp4nagios.org/de/pnp-0.6/webfe
-
-    # this doesn't work, see manifests/pnp4nagios.pp
-
-    #nagios_host { 'host-pnp':
-    #    action_url => '/pnp4nagios/index.php/graph?host=$HOSTNAME$&srv=_HOST_',
-    #    register => 0,
-    #    #ensure => absent;
-    #}
-
-    #nagios_service { 'service-pnp':
-    #    #naginatorname => 'service-pnp',
-    #    action_url => '/pnp4nagios/index.php/graph?host=$HOSTNAME$&srv=$SERVICEDESC$',
-    #    register => 0,
-    #    ensure => absent;
-    #}
 }
--- manifests/pnp4nagios.pp
+++ manifests/pnp4nagios.pp
@@ -5,9 +5,9 @@ class nagios::pnp4nagios {
             ensure => installed }
 
 
-  # unfortunatly i didn't find a way to use nagios_host and nagios_service
-  # definition, because imho puppet can't handle the "name" variable
-  # needed in these 2 definitions so we need to copy a file here.
+  # unfortunatly we can't use the nagios_host and nagios_service
+  # definition to define templates, so we need to copy a file here.
+  # see http://projects.reductivelabs.com/issues/1180 for this limitation
 
   file { 'pnp4nagios-templates.cfg':
     path   => "${nagios::defaults::vars::int_cfgdir}/conf.d/pnp4nagios-templates.cfg",
