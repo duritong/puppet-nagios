bfa099d32fd1155f9fe9d7bde2e927ea31ea3e2b
mh <mh@immerda.ch>
2013-08-22 18:18:16 +0200

    make it possible to pass two different checks for http and https

--- manifests/service/http.pp
+++ manifests/service/http.pp
@@ -16,12 +16,20 @@ define nagios::service::http(
         'absent' => $name,
         default => $check_domain
     }
+    if is_hash($check_code) {
+      $check_code_hash = $check_code
+    } else {
+      $check_code_hash = {
+        http  => $check_code,
+        https => $check_code,
+      }
+    }
     case $ssl_mode {
         'force',true,'only': {
             nagios::service{"https_${name}":
                 ensure => $ensure,
                 use => $use,
-                check_command => "check_https_url_regex!${real_check_domain}!${check_url}!'${check_code}'",
+                check_command => "check_https_url_regex!${real_check_domain}!${check_url}!'${check_code_hash[https]}'",
             }
             case $ssl_mode {
                 'force': {
@@ -39,7 +47,7 @@ define nagios::service::http(
             nagios::service{"http_${name}":
                 ensure => $ensure,
                 use => $use,
-                check_command => "check_http_url_regex!${real_check_domain}!${port}!${check_url}!'${check_code}'",
+                check_command => "check_http_url_regex!${real_check_domain}!${port}!${check_url}!'${check_code_hash[http]}'",
             }
         }
     }
