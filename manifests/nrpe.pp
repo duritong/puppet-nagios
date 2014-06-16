16814be3f41f3fbd8d283ede2c247033ecb8680a
Fredrik Thulin <fredrik@thulin.net>
2014-06-16 13:13:51 +0200

    In the future, undef is different from empty string.

--- manifests/nrpe.pp
+++ manifests/nrpe.pp
@@ -6,15 +6,15 @@ class nagios::nrpe(
   if $nagios_manage_nrpe {
     case $operatingsystem {
         'FreeBSD': {
-            if $nagios_nrpe_cfgdir == '' { $nagios_nrpe_cfgdir = '/usr/local/etc' }
-            if $nagios_nrpe_pid_file == '' { $nagios_nrpe_pid_file = '/var/spool/nagios/nrpe2.pid' }
-            if $nagios_plugin_dir == '' { $nagios_plugin_dir = '/usr/local/libexec/nagios' }
+            if $nagios_nrpe_cfgdir == '' or ! $nagios_nrpe_cfgdir { $nagios_nrpe_cfgdir = '/usr/local/etc' }
+            if $nagios_nrpe_pid_file == '' or ! $nagios_nrpe_pid_file { $nagios_nrpe_pid_file = '/var/spool/nagios/nrpe2.pid' }
+            if $nagios_plugin_dir == '' or ! $nagios_plugin_dir { $nagios_plugin_dir = '/usr/local/libexec/nagios' }
 
             include nagios::nrpe::freebsd
         }
         default: {
-            if $nagios_nrpe_pid_file == '' { $nagios_nrpe_pid_file = '/var/run/nrpe.pid' }
-            if $nagios_plugin_dir == '' { $nagios_plugin_dir = '/usr/lib/nagios/plugins' }
+            if $nagios_nrpe_pid_file == '' or ! $nagios_nrpe_pid_file { $nagios_nrpe_pid_file = '/var/run/nrpe.pid' }
+            if $nagios_plugin_dir == '' or ! $nagios_plugin_dir { $nagios_plugin_dir = '/usr/lib/nagios/plugins' }
 
             case $kernel {
                 Linux: { include nagios::nrpe::linux }
